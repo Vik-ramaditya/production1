c297a2b16a5a520cd04f5136e3cb98e084887c63
Vik-ramaditya <vikramadityagurjar3@gmail.com>
2025-05-22 20:16:53 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { application } from "express";
 dotenv.config({
     path: './env'
 })
-connectDB()
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("errror",error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+}).catch((error)=>{
+    console.log("Mongodb connection failed !!!",err);
+})
 
 // import express from "express";
-// const app = express()
-// ( async ()=>{
+// const app = exp
 //     try{
 //       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
 //       app.on("errror",(error)=>{
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class AppiResponse {
+    constructor(statusCode,data,message="success")
+    {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode<400
+    }
+}
